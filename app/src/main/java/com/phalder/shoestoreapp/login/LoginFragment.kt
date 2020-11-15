95d54d98fa128b1fe86b5e5b430a7ec9acf15d5a
Prashanta Halder <phalder@blackberry.com>
2020-11-15 12:16:10 -0800

    Welcome Screen done

--- app/src/main/java/com/phalder/shoestoreapp/login/LoginFragment.kt
+++ app/src/main/java/com/phalder/shoestoreapp/login/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.phalder.shoestoreapp.R
@@ -27,6 +28,10 @@ class LoginFragment : Fragment() {
         binding.loginButton.setOnClickListener {view: View ->
             view.findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
         }
+        binding.loginExisting.setOnClickListener {view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
+        (activity as AppCompatActivity).supportActionBar?.title = "Login"
         return binding.root
     }
 
