53362ecac65193aee4ea91dd1a6b8c004f075a8a
Prashanta Halder <phalder@blackberry.com>
2020-11-23 15:27:38 -0800

    Added ActionBar support

--- app/src/main/java/com/phalder/shoestoreapp/MainActivity.kt
+++ app/src/main/java/com/phalder/shoestoreapp/MainActivity.kt
@@ -2,18 +2,31 @@ package com.phalder.shoestoreapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_ShoeStoreApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.loginFragment,R.id.welcomeFragment,R.id.shoeListFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
 
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
